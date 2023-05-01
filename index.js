37a8e3583b27255ad3e9d47b22b340949e0f944c
ShivamSingh7280 <shivamsingh7280@gmail.com>
2023-05-01 16:50:42 +0530

    card design updated & font-size adjusted

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ quotebtn.addEventListener("click", randomQuote);
 //=============================swipper================================================
 
 var swiper = new Swiper(".mySwiper", {
-	slidesPerView: 3,
-	centeredSlides: true,
+	slidesPerView: 4,
+	centeredSlides: false,
 	spaceBetween: 30,
 	pagination: {
 		el: ".swiper-pagination",

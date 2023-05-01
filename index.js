de7c90a37cccecfd7dea94639b0b75804f0e132c
ShivamSingh7280 <shivamsingh7280@gmail.com>
2023-05-01 16:01:38 +0530

    card slider added

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-import { projectsList } from "./projectsList";
-let quoteText = document.getElementsByClassName("quote");
-let quotebtn = document.getElementById("nextQuoteBtn");
+// import { projectsList } from "./projectsList";
+const quoteText = document.getElementsByClassName("quoteContent");
+const authorName = document.getElementsByClassName("name");
+const quotebtn = document.getElementById("nextQuoteBtn");
+const soundBtn = document.getElementById("sound");
+const copyBtn = document.getElementById("copy");
+const twitterBtn = document.getElementById("twitter");
 
-function randomQuote() {
-	console.log("randomQuote Generated");
-}
+let randomQuote = async () => {
+	try {
+		quotebtn.classList.add("loading");
+		quotebtn.textContent = "Loading Quote...";
+		const response = await fetch("https://api.quotable.io/random");
+		const jsonData = await response.json();
+		const newQuote = jsonData.content;
+		const newAuthor = jsonData.author;
+		quoteText[0].textContent = newQuote;
+		authorName[0].textContent = newAuthor;
+		quotebtn.textContent = "New Quote";
+		quotebtn.classList.remove("loading");
+	} catch (error) {
+		console.log("Error found in randomQuote function", error);
+	}
+};
+
+soundBtn.addEventListener("click", () => {
+	let utterence = new SpeechSynthesisUtterance(
+		`${quoteText[0].textContent} By ${authorName[0].textContent}`
+	);
+	// utterence.pitch = 1;
+	// utterence.rate = 1.2;
+	// utterence.volume = 1;
+	speechSynthesis.speak(utterence);
+});
+
+copyBtn.addEventListener("click", () => {
+	navigator.clipboard.writeText(quoteText[0].textContent);
+});
+
+twitterBtn.addEventListener("click", () => {
+	let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText[0].textContent}`;
+	window.open(tweetUrl, "_blank");
+});
 
 quotebtn.addEventListener("click", randomQuote);
+
+//=============================swipper================================================
+
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 3,
+	centeredSlides: true,
+	spaceBetween: 30,
+	pagination: {
+		el: ".swiper-pagination",
+		type: "fraction",
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
